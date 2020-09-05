d9a8e2a67cc8352174c444cdb99f553fc38915b6
paalandreeas <pamorho@gmail.com>
2020-09-05 14:19:03 +0200

    Started creating ArtDisplay.js. Not yet completed. Added two artworks, RadiantBaby.js and BestBuddies.js. Not yet completed

---
+++ exhibition/src/components/ArtDisplay/ArtDisplay.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./ArtDisplay.css";
+import "./ArtWorks/RadiantBaby";
+import RadiantBaby from "./ArtWorks/RadiantBaby";
+import BestBuddies from "./ArtWorks/BestBuddies";
+
+class ArtDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentIndex: 0,
+            artworks: [<RadiantBaby />, <BestBuddies />],
+        };
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+    }
+
+    next() {
+        if (this.state.currentIndex === this.state.artworks.length - 1) {
+            this.setState({ currentIndex: 0 });
+        } else {
+            const newCurrentIndex = this.state.currentIndex + 1;
+            this.setState({ currentIndex: newCurrentIndex });
+        }
+    }
+
+    previous() {
+        if (this.state.currentIndex === 0) {
+            this.setState({ currentIndex: this.state.artworks.length - 1 });
+        } else {
+            const newCurrentIndex = this.state.currentIndex - 1;
+            this.setState({ currentIndex: newCurrentIndex });
+        }
+    }
+
+    render() {
+        return (
+            <div id="artContainer" onClick={this.next}>
+                {this.state.artworks[this.state.currentIndex]}
+            </div>
+        );
+    }
+}
+
+export default ArtDisplay;
