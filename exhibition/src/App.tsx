d42b14f05f98a19b6caee82e3df2124bc869df18
Oscar <ombergan@stud.ntnu.no>
2020-09-08 14:29:23 +0200

    added a component for fetching poems from poetryDB

--- exhibition/src/App.tsx
+++ exhibition/src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import ArtDisplay from "./components/ArtDisplay/ArtDisplay";
+import PoetryDisplay from "./components/PoetryDisplay/PoetryDisplay";
+
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <ArtDisplay />
+                <PoetryDisplay />
             </header>
         </div>
     );
