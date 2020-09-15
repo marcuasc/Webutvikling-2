5c5b86f7406ee463be99089f4876d6476fb16af8
paalandreeas <pamorho@gmail.com>
2020-09-15 13:44:45 +0200

    Improved layout. Closes #6

--- exhibition/src/components/PoetryDisplay/PoetryDisplay.tsx
+++ exhibition/src/components/PoetryDisplay/PoetryDisplay.tsx
@@ -1,40 +1,47 @@
 import React from "react";
+import "./PoetryDisplay.css";
 
-let input = ["https://poetrydb.org/title/Ozymandias/lines.json", "https://poetrydb.org/title/\"Heavenly Father\" -- take to thee/lines.json"];
+let input = [
+    "https://poetrydb.org/title/Ozymandias/lines.json",
+    'https://poetrydb.org/title/"Heavenly Father" -- take to thee/lines.json',
+];
 
 interface AppState {
     data: Array<any>;
 }
 
-interface AppProps {
-
-}
-
+interface AppProps {}
 
 class PoetryDisplay extends React.Component<AppProps, AppState> {
     constructor(props: AppProps) {
         super(props);
         this.state = {
-            data: []
-        }
+            data: [],
+        };
     }
 
     componentDidMount(): void {
         fetch(input[1])
             .then((response) => response.json())
-            .then(poem => {
-                this.setState({data: poem[0].lines})
+            .then((poem) => {
+                this.setState({ data: poem[0].lines });
             });
     }
 
+    getText() {
+        let text = "";
+        for (let e in this.state.data) {
+            text += this.state.data[e] + " ";
+        }
+        return text;
+    }
 
     render() {
         return (
             <div>
-                {this.state.data.map((item, index) => <p key={item}>{item}</p>)}
+                <p>{this.getText()}</p>
             </div>
         );
-
     }
 }
 
