8d2b01b34ca87f8901116b014eaa4d67853ac5be
paalandreeas <pamorho@gmail.com>
2020-09-28 19:56:45 +0200

    Added poems and audiofiles specific for artworks. Updates #14

--- exhibition/src/components/AudioDisplay/AudioDisplay.tsx
+++ exhibition/src/components/AudioDisplay/AudioDisplay.tsx
@@ -4,18 +4,80 @@ import "./AudioDisplay.css";
 
 // Links to the different mp3-files used in the project
 let input = [
-    "./resources/AudioFiles/alien.mp3",
-    "./resources/AudioFiles/birds.mp3",
-    "./resources/AudioFiles/waterfall.mp3",
+    // BestBuddies
+    [
+        "./resources/AudioFiles/laugh.mp3",
+        "./resources/AudioFiles/sad.mp3",
+        "./resources/AudioFiles/cheering.mp3",
+    ],
+    // CrackDown
+    [
+        "./resources/AudioFiles/hardShoes.mp3",
+        "./resources/AudioFiles/march.mp3",
+        "./resources/AudioFiles/walkGravel.mp3",
+    ],
+    // Dance
+    [
+        "./resources/AudioFiles/bounce.mp3",
+        "./resources/AudioFiles/cartoonHop.mp3",
+        "./resources/AudioFiles/tapDance.mp3",
+    ],
+    // DJ
+    [
+        "./resources/AudioFiles/DJScratch.mp3",
+        "./resources/AudioFiles/boo.mp3",
+        "./resources/AudioFiles/partyPeople.mp3",
+    ],
+    // Dogs
+    [
+        "./resources/AudioFiles/howl.mp3",
+        "./resources/AudioFiles/labBark.mp3",
+        "./resources/AudioFiles/smallBark.mp3",
+    ],
+    // Earth
+    [
+        "./resources/AudioFiles/alien.mp3",
+        "./resources/AudioFiles/birds.mp3",
+        "./resources/AudioFiles/waterfall.mp3",
+    ],
+    // Football
+    [
+        "./resources/AudioFiles/cheer.mp3",
+        "./resources/AudioFiles/crowd.mp3",
+        "./resources/AudioFiles/horn.mp3",
+    ],
+    // Heart
+    [
+        "./resources/AudioFiles/heartbeat.mp3",
+        "./resources/AudioFiles/ECG.mp3",
+        "./resources/AudioFiles/surgery.mp3",
+    ],
+    // RadiantBaby
+    [
+        "./resources/AudioFiles/babyTalk.mp3",
+        "./resources/AudioFiles/giggle.mp3",
+        "./resources/AudioFiles/musicBox.mp3",
+    ],
+    // Snake
+    [
+        "./resources/AudioFiles/rattle1.mp3",
+        "./resources/AudioFiles/rattle2.mp3",
+        "./resources/AudioFiles/snip.mp3",
+    ],
 ];
 
 const AudioDisplay: React.FunctionComponent = () => {
     // Gets the values from the context
     const values: WebContextInterface = React.useContext(WebContext);
-    // Renders audio based on the value of the sound context
+    // Renders audio based on the value of the sound and index context
     return (
         <div id="audioDisplay">
-            <audio src={input[values.sound]} controls autoPlay muted />
+            <audio
+                src={input[values.index][values.sound]}
+                controls
+                autoPlay
+                muted
+            />
         </div>
     );
 };
--- exhibition/src/components/PoetryDisplay/PoetryDisplay.tsx
+++ exhibition/src/components/PoetryDisplay/PoetryDisplay.tsx
@@ -3,43 +3,120 @@ import WebContext, { WebContextInterface } from "../../WebContext";
 import "./PoetryDisplay.css";
 
 // Defines the list of poems
-let input = [
-    "https://poetrydb.org/title/A Fragment: To Music/lines.json",
-    "https://poetrydb.org/title/Fragment: 'The Death Knell Is Ringing'/lines.json",
-    "https://poetrydb.org/title/Dying at my music!/lines.json",
+let poems = [
+    // BestBuddies
+    [
+        "https://poetrydb.org/title/Song—Love in the Guise of Friendship",
+        "https://poetrydb.org/title/Fragment: To a Friend Released From Prison",
+        "https://poetrydb.org/title/Impromptu, in Reply to a Friend",
+    ],
+    // CrackDown
+    [
+        "https://poetrydb.org/title/A Flower will not trouble her, it has so small a Foot",
+        "https://poetrydb.org/title/When We Two Walked",
+        "https://poetrydb.org/title/Fragment: 'And That I Walk Thus Proudly Crowned'",
+    ],
+    // Dance
+    [
+        "https://poetrydb.org/title/The Baby's Dance",
+        "https://poetrydb.org/title/The Night Dance",
+        "https://poetrydb.org/title/395. Sonnet on the Author’s Birthday",
+    ],
+    // DJ
+    [
+        "https://poetrydb.org/title/A Fragment: To Music",
+        "https://poetrydb.org/title/Another Fragment: To Music",
+        "https://poetrydb.org/title/Away With Funeral Music",
+    ],
+    // Dogs
+    [
+        "https://poetrydb.org/title/To a Lady with an Unruly and Ill-mannered Dog Who Bit several Persons of Importance",
+        "https://poetrydb.org/title/Inscription on the Monument of a Newfoundland Dog",
+        "https://poetrydb.org/title/421. Epitaph on a Lap-dog",
+    ],
+    // Earth
+    [
+        "https://poetrydb.org/title/Earth's Answer",
+        "https://poetrydb.org/title/Earth's Eternity",
+        "https://poetrydb.org/title/Holy Sonnet VII: At The Round Earth's Imagined Corners Blow",
+    ],
+    // Football
+    [
+        "https://poetrydb.org/title/Sonnet 36: Let me confess that we two must be twain",
+        "https://poetrydb.org/title/Bliss is the plaything of the child --",
+        "https://poetrydb.org/title/The Charity Ball",
+    ],
+    // Heart
+    [
+        "https://poetrydb.org/title/123. Lines to an Old Sweetheart",
+        "https://poetrydb.org/title/For largest Woman's Hearth I knew",
+        "https://poetrydb.org/title/His Heart was darker than the starless night",
+    ],
+    //RadiantBaby
+    [
+        "https://poetrydb.org/title/Baby Charley.",
+        "https://poetrydb.org/title/The Child Is Father To The Man",
+        "https://poetrydb.org/title/To a Young Child",
+    ],
+    // Snake
+    [
+        "https://poetrydb.org/title/Lucifer in Starlight",
+        "https://poetrydb.org/title/Sonnet LXI: Since There's No Help",
+        "https://poetrydb.org/title/The Frightened Ploughman",
+    ],
 ];
 
 const PoetryDisplay: React.FunctionComponent = () => {
     // Import the context values
     const values: WebContextInterface = React.useContext(WebContext);
     // Declares data as a list of strings in the components state
-    let [data, setData] = React.useState([""]);
+    let [data, setData] = React.useState({
+        author: "",
+        title: "",
+        lines: [""],
+    });
 
     // useEffect triggers when the component loads and when values.poetry updates.
     useEffect(() => {
         // sets data to loading
-        setData(["Loading poem..."]);
-        // fetches from poetryDB on the current poetry value
-        fetch(input[values.poetry])
+        setData({
+            author: "loading",
+            title: "loading",
+            lines: ["loading"],
+        });
+        // fetches from poetryDB on the current poetry and index value
+        fetch(poems[values.index][values.poetry])
             .then((response) => response.json())
             .then((poem) => {
-                setData(poem[0].lines);
+                // Sets the data to the correct params
+                setData({
+                    author: poem[0].author,
+                    title: poem[0].title,
+                    lines: poem[0].lines,
+                });
             });
-    }, [values.poetry]);
-
-    // gets the text from data and puts it in a string and returns it
-    function getText() {
-        let text = "";
-        for (let e in data) {
-            text += data[e] + " ";
-        }
-        return text;
-    }
+    }, [values.poetry, values.index]);
 
     return (
-        <div>
-            <p>{getText()}</p>
-        </div>
+        <p>
+            {/* Puts the title and author in corresponding spans */}
+            <span className="bold">{data.title}</span>
+            <br />
+            <span className="cursive">{data.author}</span>
+            <br />
+
+            {/* gets the lines from data and puts them in a span element with br */}
+            {data.lines.map((line, index) =>
+                line !== "" ? (
+                    <span key={index}>
+                        {line}
+                        <br />
+                    </span>
+                ) : (
+                    ""
+                )
+            )}
+        </p>
     );
 };
 
